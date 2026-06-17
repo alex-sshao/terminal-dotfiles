35685f65a301fceb34ac1f147384ebe34b058a79
Alex <alexs237044@gmail.com>
2026-06-17 00:49:30 -0400

    feat: Adding powerline font install

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 mv .git git
 cp -r .* ..
 mv git .git
+git clone https://github.com/powerline/fonts.git --depth=1
+./fonts/install.sh
+rm -rf ./fonts
