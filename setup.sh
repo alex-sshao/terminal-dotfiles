6d19a7b258ef5cd0e1e844c1d564a2a57075bfde
shao <alexs237044@gmail.com>
2026-06-30 03:32:29 -0400

    fix: Setup clean script rewrite

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+
+bd="$( cd "$( dirname "$0" )" && pwd )/"
+link(){
+	echo "Linking ${bd}$1 to $2"
+	ln -sf ${bd}$1 $2
+	if [ $? -eq 0 ]; then
+		echo Success!
+	else
+		echo Fail!
+	fi
+}
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp -r .vim ~
-ln -sf "$(exec pwd)"/.vimrc ~/.vimrc
-ln -sf "$(exec pwd)"/.zsh ~/.zsh
+
+link ".vimrc" ~/.vimrc
+link ".zshrc" ~/.zshrc
+
 git clone https://github.com/powerline/fonts.git --depth=1
 ./fonts/install.sh
 rm -rf ./fonts
